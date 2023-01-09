a80d66c401c5d1bd7f330722627e44498fb2d612
Andre Backman <abackman@student.hive.fi>
2023-01-09 16:23:13 +0200

    create assembler.h

---
+++ includes/assembler.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   assembler.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 15:11:32 by abackman          #+#    #+#             */
+/*   Updated: 2023/01/09 16:22:45 by abackman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ASSEMBLER_H
+# define  ASSEMBLER_H
+# include "libft.h"
+# include "op.h"
+# include <stdint.h>
+
+
+/*
+** Structs
+*/
+
+typedef struct s_asm
+{
+	header_t	head;
+}	t_asm;
+
+#endif
