2457a96ae927ba0fd143a5ac754da85e9b98582c
Pyry-Peetri Piironen <pyry13@Pyry-Peetri-MacBook-Pro.local>
2023-01-20 14:51:33 +0200

    Fixed some error messages.

--- srcs/vm/prerequites/read_cor.c
+++ srcs/vm/prerequites/read_cor.c
@@ -19,17 +19,17 @@ void	read_magic(int fd)
 	int				total;
 
 	if (read(fd, &buf, 4) != 4)
-		print_error("Invalid file.", 0);
+		print_error("Invalid file. .cor file is too short. [Magic code]", 0);
 	total = (buf[0] << 24) + (buf[1] << 16) + (buf[2] << 8) + buf[3];
 	if (total != COREWAR_EXEC_MAGIC)
-		print_error("Invalid file.", 0);
+		print_error("Invalid file. Magic code is invalid.", 0);
 }
 
 // Read name of champion from .cor file and check the validity.
 void	read_name(t_player *champ, int fd)
 {
 	if (read(fd, champ->name, PROG_NAME_LENGTH) != PROG_NAME_LENGTH)
-		print_error("Invalid file.", 0);
+		print_error("Invalid file. .cor file is too short. [Name]", 0);
 	champ->name[PROG_NAME_LENGTH] = '\0';
 }
 
@@ -41,11 +41,11 @@ void	skip_nulls(int fd)
 
 	i = 0;
 	if (read(fd, &buf, 4) != 4)
-		print_error("Invalid file.", 0);
+		print_error("Invalid file. .cor file is too short. [NULLs]", 0);
 	while (i < 4)
 	{
 		if (buf[i])
-			print_error("Invalid file.", 0);
+			print_error("Invalid file.  No NULLs on right place.", 0);
 		i++;
 	}
 }
@@ -56,7 +56,7 @@ void	read_excode_size(t_player *champ, int fd)
 	unsigned char	buf[4];
 
 	if(read(fd, &buf, 4) != 4)
-		print_error("Invalid file.", 0);
+		print_error("Invalid file. .cor file is too short. [Excode size]", 0);
 	champ->code_size = (buf[0] << 24) + (buf[1] << 16) + (buf[2] << 8) + buf[3];
 
 }
@@ -65,7 +65,7 @@ void	read_excode_size(t_player *champ, int fd)
 void	read_comment(t_player *champ, int fd)
 {
 	if (read(fd, champ->comment, COMMENT_LENGTH) != COMMENT_LENGTH)
-		print_error("Invalid file.", 0);
+		print_error("Invalid file. .cor file is too short. [Comment]", 0);
 	champ->comment[COMMENT_LENGTH] = '\0';
 
 }
