763bc82660ef246279e1ccf0800e248e20062901
Andre Backman <abackman@student.hive.fi>
2023-01-24 14:30:42 +0200

    update ft_strstr

--- libft/str/ft_strstr.c
+++ libft/str/ft_strstr.c
@@ -3,24 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ppiirone <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 18:34:52 by ppiirone          #+#    #+#             */
-/*   Updated: 2021/11/08 18:35:09 by ppiirone         ###   ########.fr       */
+/*   Updated: 2023/01/24 14:30:00 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
+static	int	ft_checkstr(const char *h, const char *n, size_t i, size_t j)
+{
+	if (n[j] == '\0')
+		return (0);
+	while (h[i] != '\0')
+	{
+		j = 0;
+		if (h[i] == n[j])
+		{
+			while (h[i + j] == n[j])
+			{
+				j++;
+				if (n[j] == '\0')
+					return (i);
+			}
+		}
+		i++;
+	}
+	return (-1);
+}
+
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	if (ft_strlen(needle) == 0)
-		return ((char *)(haystack));
-	while (ft_strlen(haystack) >= ft_strlen(needle))
-	{
-		if (ft_strncmp(haystack, needle, ft_strlen(needle)) == 0)
-			return ((char *)(haystack));
-		haystack++;
-	}
-	return (NULL);
+	int		res;
+
+	res = ft_checkstr(haystack, needle, 0, 0);
+	if (res == 0 || needle == NULL)
+		return ((char *)haystack);
+	if (res > 0)
+		return ((char *)&haystack[res]);
+	else
+		return (NULL);
 }
