28074d738e22983d96e59784bb1a4d0e08f30f4e
Andre home <andrebackmann@gmail.com>
2023-02-06 18:17:15 +0200

    create asm_token_error()

--- srcs/asm/error_asm.c
+++ srcs/asm/error_asm.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_asm.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abackman <abackman@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 15:26:37 by abackman          #+#    #+#             */
-/*   Updated: 2023/01/30 19:20:55 by abackman         ###   ########.fr       */
+/*   Updated: 2023/02/06 17:24:19 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,13 @@ static void	asm_syntax_error(t_asm *d, int status)
 		ft_printf("Syntax error at [%d:%d] EOF\n", d->row, d->col);
 	else
 		ft_printf("Syntax error at token [%d:%d]\n", d->row, d->col);
+}
+
+void	asm_token_error(t_asm *d, t_oken *cur)
+{
+	ft_printf("Syntax error at token [%d:%d]\n", cur->row, cur->col);
+	free_asm(d);
+	exit(EXIT_FAILURE);
 }
 
 void	error_asm(t_asm *d, char *line, int status)
