57ae0681e1f079c551cca1e5d157c09fff7da9a2
Andre Backman <abackman@student.hive.fi>
2023-02-07 17:14:12 +0200

    update error messages

--- srcs/asm/error_asm.c
+++ srcs/asm/error_asm.c
@@ -3,26 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   error_asm.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abackman <abackman@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 15:26:37 by abackman          #+#    #+#             */
-/*   Updated: 2023/02/06 17:24:19 by abackman         ###   ########.fr       */
+/*   Updated: 2023/02/07 16:22:31 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "assembler.h"
 
-static void	asm_syntax_error(t_asm *d, int status)
+static void	asm_syntax_error(t_asm *d, t_oken *cur, int status)
 {
 	if (status == EOF_ERR)
 		ft_printf("Syntax error at [%d:%d] EOF\n", d->row, d->col);
+	else if (cur)
+	{
+		if (cur->type == OP)
+			ft_printf("Syntax error at token [%d:%d] INSTRUCTION \"%s\"\n", \
+			d->row, d->col, cur->str);
+		else if (cur->type == REG)
+			ft_printf("Syntax error at token [%d:%d] REGISTER \"%s\"\n", \
+			d->row, d->col, cur->str);
+		else if (cur->type == LABEL)
+			ft_printf("Syntax error at token [%d:%d] INDIRECT_LABEL \"%s\"\n", \
+			d->row, d->col, cur->str);
+		else if (cur->type == DIR)
+			ft_printf("Syntax error at token [%d:%d] DIRECT \"%s\"\n", \
+			d->row, d->col, cur->str);
+		else if (cur->type == IND)
+			ft_printf("Syntax error at token [%d:%d] INDIRECT \"%s\"\n", \
+			d->row, d->col, cur->str);
+		else if (cur->type == DIRLAB)
+			ft_printf("Syntax error at token [%d:%d] DIRECT_LABEL \"%s\"\n", \
+			d->row, d->col, cur->str);
+	}
 	else
 		ft_printf("Syntax error at token [%d:%d]\n", d->row, d->col);
 }
 
-void	asm_token_error(t_asm *d, t_oken *cur)
+void	asm_token_error(t_asm *d, t_oken *cur, int status)
 {
-	ft_printf("Syntax error at token [%d:%d]\n", cur->row, cur->col);
+	if (status == NAMELEN_ERR)
+		ft_putstr_fd(NAME_ERR, STDERR_FILENO);
+	else if (status == COMMLEN_ERR)
+		ft_putstr_fd(COMMENT_ERR, STDERR_FILENO);
+	else
+		asm_syntax_error(d, cur, status);
 	free_asm(d);
 	exit(EXIT_FAILURE);
 }
@@ -33,7 +59,7 @@ void	error_asm(t_asm *d, char *line, int status)
 	if (status == LEX_ERR)
 		ft_printf("Lexical error at [%d:%d]\n", d->row, d->col);
 	else if (status == STX_ERR || status == EOF_ERR)
-		asm_syntax_error(d, status);
+		asm_syntax_error(d, NULL, status);
 	if (line)
 		ft_strdel(&line);
 	free_asm(d);
