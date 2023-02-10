ef5cd357c560459ca0da5a41efc839bcfebb3406
matiasjokela <matias.jokela@outlook.com>
2023-02-10 13:54:54 +0200

    op_sti tested

--- srcs/vm/main/main.c
+++ srcs/vm/main/main.c
@@ -24,30 +24,17 @@ int	main(int argc, char **argv)
 	init_core(argc, argv, d);
 
 
-	// TEST OP_STI!!!!
-	// TEST OP_STI!!!!
-	// TEST OP_STI!!!!
-	// TEST OP_STI!!!!
-	// TEST OP_STI!!!!
-	// TEST OP_STI!!!!
-	// TEST OP_STI!!!!
-	// TEST OP_STI!!!!
-
-
-	
-	// reg - ind - reg
-
-	d->arena[4084] = 0xe4;		// argument value code
-	d->arena[4085] = 0x00;		// reg 1
-	d->arena[4086] = 0x08;		// IND 1
-	d->arena[4087] = 0x02;		// IND 2
-	d->arena[4088] = 0x96;		// reg 3
-	d->arena[4089] = 0x0a; 		// reg
-	d->arena[4090] = 0xc8;		// end of second
+	d->arena[4084] = 0x78;		// argument value code
+	d->arena[4085] = 0x01;		// 
+	d->arena[4086] = 0x00;		// 
+	d->arena[4087] = 0x07;		// 
+	d->arena[4088] = 0x00;		// 
+	d->arena[4089] = 0x64; 		// 
+	d->arena[4090] = 0x00;		// 
 	
 	d->arena[4091] = 0x00; 		// reg
-	d->arena[4092] = 0x00;		// 
-	d->arena[4093] = 0x00; 		//  reg
+	d->arena[4092] = 0x02;		// 
+	d->arena[4093] = 0x64; 		//  reg
 	d->arena[4094] = 0x32;		//
 
 
@@ -59,10 +46,10 @@ int	main(int argc, char **argv)
 	d->arena[288] = 0x00;
 	d->arena[289] = 0x00;
 	d->arena[290] = 0x00;
-	d->arena[187 + 512] = 0x7f;
-	d->arena[188 + 512] = 0xff;
-	d->arena[189 + 512] = 0xff;
-	d->arena[190 + 512] = 0xff;
+	// d->arena[187 + 512] = 0x7f;
+	// d->arena[188 + 512] = 0xff;
+	// d->arena[189 + 512] = 0xff;
+	// d->arena[190 + 512] = 0xff;
 
 
 
@@ -70,24 +57,24 @@ int	main(int argc, char **argv)
 	d->carriage_head->carry = 0;
 
 	d->carriage_head->registrys[0] = 50;
-	d->carriage_head->registrys[1] = 150;
+	d->carriage_head->registrys[1] = 100;
 	d->carriage_head->registrys[2] = 2100000;
-	d->carriage_head->registrys[3] = 44;
+	d->carriage_head->registrys[3] = 612;
 
 
 	
-
+	print_arena(d);
 	print_carriages(d);
-	//op_st(d->carriage_head, d);
+	op_sti(d->carriage_head, d);
 	//op_jump(d->carriage_head, d);
 	//op_add(d->carriage_head, d);
-	op_lldi(d->carriage_head, d);
+	//op_lldi(d->carriage_head, d);
 
 
 	ft_printf("{red} *	*	*	*	*	*	*	*	*	*	\n");
 	//print_arena(d);
 	print_carriages(d);
-	//print_arena(d);
+	print_arena(d);
 
 	ft_printf("{green}Success! Main exit (exit code 0).\n");
 	exit (0);
