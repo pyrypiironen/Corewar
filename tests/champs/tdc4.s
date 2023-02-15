ce745b842b4daa05f9201bdb751df8fb5315b3a3
Andre Backman <abackman@student.hive.fi>
2023-02-15 17:43:29 +0200

    update tests

--- tests/champs/tdc4.s
+++ tests/champs/tdc4.s
@@ -1,6 +1,6 @@
 .name 		"dtonc v1.0pl4"
 .comment	"ta mere elle fait du rodeo sur les kfar"
-.extend
+#.extend
 
 
 debut:	
--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,14 @@
-./asm_orig champs/Car.s champs/barriere.s champs/Gagnant.s
-./corewar_orig -n champs/Gagnant.cor champs/Car.cor
+#!/bin/bash
+
+rm tmp1.cor tmp2.cor
+
+cd .. && make && cd tests
+
+cp $1 tmp1.s
+cp $1 tmp2.s
+./asm_orig tmp1.s
+../asm tmp2.s
+diff -u tmp1.cor tmp2.cor
+rm tmp1.s tmp2.s
+#./asm_orig champs/Car.s champs/barriere.s champs/Gagnant.s
+#./corewar_orig -n champs/Gagnant.cor champs/Car.cor
