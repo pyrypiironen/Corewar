06b7b14c87663280d89ce2ab86242d1754ce0d92
Andre Backman <abackman@student.hive.fi>
2023-02-15 17:43:00 +0200

    calculations correct

--- srcs/asm/translate_calculations.c
+++ srcs/asm/translate_calculations.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 10:25:30 by abackman          #+#    #+#             */
-/*   Updated: 2023/02/14 17:58:34 by abackman         ###   ########.fr       */
+/*   Updated: 2023/02/15 17:20:28 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,15 +48,13 @@ static void	add_label_values(t_asm *d)
 			if (name)
 			{
 				tmplab = get_label(d, name, ft_strlen(name));
-				if (tmplab && tmplab->statement)
-					tmp->args[i] = tmplab->statement->location - tmp->location;
-				else
+				if (!tmplab || !tmplab->statement)
 					tmp->args[i] = d->code_size;
-				//ft_printf("%p %s %i\n", tmp, name, tmp->args[i]);
+				else
+					tmp->args[i] = tmplab->statement->loc - tmp->loc;
 			}
 			i++;
 		}
-				//write(1, "\n", 1);
 		tmp = tmp->next;
 	}
 }
@@ -66,7 +64,6 @@ static size_t	calculate_rescode(t_stat *cur)
 	cur->rescode = cur->argtypes[0] << 6;
 	cur->rescode |= cur->argtypes[1] << 4;
 	cur->rescode |= cur->argtypes[2] << 2;
-	//ft_printf("calculate rescode [%u] [%x]\n", cur->opcode + 1, cur->rescode);
 	return (1);
 }
 
@@ -93,7 +90,6 @@ static size_t	calculate_statement_size(t_stat *cur)
 			ret += 1;
 		i++;
 	}
-	//ft_printf("SIZE %u\n", ret);
 	return (ret);
 }
 
@@ -103,15 +99,15 @@ void	translate_calculations(t_asm *d)
 	t_stat	*tmp;
 
 	tmp = d->statements;
+	tmp->loc = 0;
 	while (tmp)
 	{
 		size = 0;
 		size = calculate_statement_size(tmp);
-		if (tmp->next)
-			tmp->next->location = size;
 		d->code_size += size;
+		if (tmp->next)
+			tmp->next->loc = d->code_size;
 		tmp = tmp->next;
 	}
-	ft_printf("code_size: %%d %u | %%x %x\n", d->code_size, d->code_size);
 	add_label_values(d);
 }
