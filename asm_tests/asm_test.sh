4e930230890ba00761c56b4780b871239874652b
Andre Backman <abackman@student.hive.fi>
2023-03-01 12:31:23 +0200

    update test script

--- asm_tests/asm_test.sh
+++ asm_tests/asm_test.sh
@@ -11,22 +11,10 @@ TIME='0.1'
 cd .. && make asm && cd asm_tests
 if [ "$1" != "" ]; then
 	if test -d "$1"; then
-		find "$1" -name "*.s" -type f -exec bash compare_s_file.sh {} \;
+		find "$1" -name "*.s" -type f -exec bash compare_file.sh {} \;
 	else
 		printf "usage: bash %s <directory containing .s files>\n" "$0"
 	fi
 else
 	printf "usage: bash %s <directory containing .s files>\n" "$0"
 fi
-#if [ "$1" != "" ]; then
-#	if test -d $1; then
-#		for file in $1/*
-#		do
-#			bash compare_s_file.sh $file
-#		done
-#	else
-#	printf "usage: bash "$0" <directory containing .s files>\n"
-#	fi
-#else
-#	printf "usage: bash "$0" <directory containing .s files>\n"
-#fi
